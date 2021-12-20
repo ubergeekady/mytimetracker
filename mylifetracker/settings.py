b66f71a5554d90ea97e4bf3304505bedaee64e65
Aditya Singh <aditya@Adityas-MacBook-Pro.local>
2021-12-20 20:59:56 +0530

    Deploy

--- mylifetracker/settings.py
+++ mylifetracker/settings.py
@@ -28,11 +28,7 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-if DEBUG:
-    environ.Env.read_env(os.path.join(BASE_DIR, '.env.local'))
-else:
-    environ.Env.read_env(os.path.join(BASE_DIR, '.env.prod'))
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 if DEBUG:
     SECRET_KEY = 'secret'
